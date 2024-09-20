b50e7032cd860db8d973544e96afd59326bee635
HoRichard <richardho16@hotmail.com>
2024-09-19 19:20:23 -0700

    pushing to work on desktop

--- src/code/ca/bcit/comp2522/lab03/IDevice.java
+++ src/code/ca/bcit/comp2522/lab03/IDevice.java
@@ -3,7 +3,36 @@ package ca.bcit.comp2522.lab03;
 /**
  *
  */
-public class IDevice
+public abstract class IDevice
 {
 
+    private final String purpose;
+
+    IDevice(final String purpose)
+    {
+        this.purpose = purpose;
+    }
+
+    String getPurpose()
+    {
+        return this.purpose;
+    }
+
+    abstract void printDetails();
+
+    @Override
+    public String toString()
+    {
+        final StringBuilder sb;
+
+        sb = new StringBuilder();
+
+        sb.append(getPurpose());
+
+        return sb.toString();
+
+    }
+
+    @Override
+    boolean equals();
 }
