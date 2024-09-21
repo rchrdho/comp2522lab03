41f347f8dad1373e18c44a2f8c4e1466a320fa18
Gem <baos2019731@gmail.com>
2024-09-20 20:55:41 -0700

    ipad class created

--- src/code/ca/bcit/comp2522/lab03/IPad.java
+++ src/code/ca/bcit/comp2522/lab03/IPad.java
@@ -11,13 +11,13 @@ public class IPad extends IDevice {
 
     private static final String IPad_PURPOSE = "learning";
 
-    private final boolean hasCase;
-    private final String  systemVersion;
+    private boolean hasCase;
+    private String  systemVersion;
 
-    public IPad(boolean hasCase,
-                String systemVersion) {
+    public IPad(final boolean hasCase,
+                final String systemVersion) {
 
-        super("learning");
+        super("IPad_PURPOSE");
         this.hasCase = hasCase;
         this.systemVersion = systemVersion;
     }
@@ -26,7 +26,7 @@ public class IPad extends IDevice {
         return hasCase;
     }
 
-    public void setHasCase(boolean hasCase) {
+    public void setHasCase(final boolean hasCase) {
         this.hasCase = hasCase;
     }
     public String getSystemVersion() {
@@ -36,10 +36,20 @@ public class IPad extends IDevice {
     public void setSystemVersion(String systemVersion) {
         this.systemVersion = systemVersion;
     }
+@Override
+public String toString() {
+    final StringBuilder sb;
+    sb = new StringBuilder();
 
+    sb.append(super.toString());
+    sb.append("\nHasCase: " + hasCase);
+    sb.append("\nIPAD system version: " + systemVersion);
+    return sb.toString();
+
+}
     @Override
     public void printDetails(){
-        System.out.println();
+        System.out.println(this.toString());
     }
 
 }
